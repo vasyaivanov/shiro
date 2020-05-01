53f9fc4d30d656a802ca28d3c395eee1c764920f
vasyaivanov <vasyaivanov@users.noreply.github.com>
2020-05-01 12:12:10 -0700

    Connect ONLY on process.env.PORT

--- server.js
+++ server.js
@@ -124,13 +124,15 @@ function startServer()
 {
   // if(process && process.evn && process.env.PORT) {
 
-    if(process.env.PORT == undefined) {
-      wigwam.listen(envar('port'), envar('host'));
-      console.log('listening on '+(envar('host') ? envar('host') + ':' : '')+envar('port'));  
-    }else {
-      wigwam.listen(process.env.PORT);
-      console.log('listening on '+ process.env.PORT);
-    }
+    wigwam.listen(process.env.PORT);
+
+    // if(process.env.PORT == undefined) {
+    //   wigwam.listen(envar('port'), envar('host'));
+    //   console.log('listening on '+(envar('host') ? envar('host') + ':' : '')+envar('port'));  
+    // }else {
+    //   wigwam.listen(process.env.PORT);
+    //   console.log('listening on '+ process.env.PORT);
+    // }
     
   // } else {
   //   wigwam.listen(envar('port'), envar('host'));
